bfc908ee1f8ca08ee104c8a67978aa8b6ee14afa
Justin Vocke <therealmathhelper@gmail.com>
2024-04-22 11:09:50 -0500

    Add basic StudentService class/interface. Add Student model. Add SchoolContext and register with DI.

---
+++ BackendTask.Business/Services/Students/StudentService.cs
@@ -0,0 +1,37 @@
+﻿using BackendTask.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendTask.Business.Services.Students
+{
+    public class StudentService : IStudentService
+    {
+        public Task AddStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteStudent(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetStudent(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Student>> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStudent(int studentId, Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BackendTask.Data/Contexts/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using BackendTask.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendTask.Data.Contexts
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
