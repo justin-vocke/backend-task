b8a1556012766c911c7b886410a90623eef79831
Justin Vocke <therealmathhelper@gmail.com>
2024-05-23 21:03:16 -0500

    Add Nunit test project

---
+++ BackendTask.Test/Tests/Business/StudentServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendTask.Test.Tests.Business
+{
+    [TestFixture]
+    public class StudentServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
